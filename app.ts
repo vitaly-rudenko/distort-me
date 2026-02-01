0434fc38e5c76406fdef622ccded6649e75b5527
Vitaly Rudenko <maurice.dederick@gmail.com>
2026-02-01 21:39:56 +0200

    Ideal parameters

--- app.ts
+++ app.ts
@@ -1,25 +1,41 @@
 import { execSync } from 'node:child_process'
 import fs from 'fs/promises'
 
+let id = 5
+
 async function extractFrames() {
   await fs.rm('./frames', { recursive: true, force: true })
   await fs.mkdir('./frames', { recursive: true })
 
-  execSync(`ffmpeg -i ./video.mp4 -r 24 ./frames/$filename%03d.jpg`)
+  execSync(`ffmpeg -i ./video_${id}.mp4 -r 24 ./frames/$filename%03d.jpg`)
 }
 
-async function distortFrames() {
+async function getFrameSize() {
+  const results = execSync(
+    `docker run --rm \
+     -v "./frames:/frames" \
+     imagemagick identify -ping -format '%wx%h' "/frames/001.jpg"`,
+  )
+
+  const dimensions = results.toString('utf-8')
+  return dimensions.split('x').map(Number) as [number, number]
+}
+
+async function distortFrames(dimensions: [number, number]) {
   await fs.rm('./distorted', { recursive: true, force: true })
   await fs.mkdir('./distorted', { recursive: true })
 
   const filenames = await fs.readdir('./frames')
   for (const [i, filename] of filenames.entries()) {
-    console.log(`Distorting frame ${i + 1} of ${filenames.length}`)
+    const percentage = i / filenames.length
+    const rescale = Math.floor(40 + 50 * (1 - percentage))
+
+    console.log(`Distorting frame ${i + 1} of ${filenames.length} ${rescale}%`)
 
     execSync(
       `docker run --rm \
-        -v "./frames:/frames" -v "./distorted:/distorted" \
-       imagemagick "/frames/${filename}" -liquid-rescale '50%' -resize '200%' "/distorted/${filename}"`,
+       -v "./frames:/frames" -v "./distorted:/distorted" \
+       imagemagick "/frames/${filename}" -liquid-rescale '${rescale}%' -resize '${dimensions[0]}x${dimensions[1]}' "/distorted/${filename}"`,
     )
   }
 }
@@ -29,16 +45,18 @@ async function combineFrames() {
 
   execSync(
     `ffmpeg -framerate 24 -pattern_type glob -i "./distorted/*.jpg" \
-     -i video.mp4 -filter:a "vibrato=f=10:d=0.7" -c:a libopus -shortest \
-     -c:v libx264 -pix_fmt yuv420p combined.mp4`,
+     -i video_${id}.mp4 -filter:a "vibrato=f=10:d=0.7" -c:a libopus -shortest \
+     -c:v libx264 -pix_fmt yuv420p combined_${id}.mp4`,
   )
 }
 
 console.log('Extracting frames...')
 await extractFrames()
 
+const dimensions = await getFrameSize()
+
 console.log('Distorting frames...')
-await distortFrames()
+await distortFrames(dimensions)
 
 console.log('Combining frames...')
 await combineFrames()
