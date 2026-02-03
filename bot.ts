f433dd37340c8ccfb22ba6a2580235da97820906
Vitaly Rudenko <maurice.dederick@gmail.com>
2026-02-03 23:04:30 +0200

    More checks

--- bot.ts
+++ bot.ts
@@ -1,14 +1,142 @@
 import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
 
 const telegraf = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!)
 
-const maxSizeBytes = 100 * 1028 * 1024 // 100 MB
-const maxDurationSeconds = 60
+const maxSizeBytes = 256 * 1028 * 1024 // 256 MB
+const maxDurationSeconds = 90
 const maxWidth = 2048
 const maxHeight = 1556
-const supportedMimeTypes = ['video/quicktime', 'video/mp4']
+const maxDiameter = Math.ceil(Math.sqrt(maxWidth ** 2 + maxHeight ** 2))
+const supportedMimeTypes = ['video/quicktime', 'video/mp4', 'audio/ogg', 'audio/mpeg']
 
-telegraf.on('video', async context => {
+telegraf.on(message('voice'), async context => {
+  const durationSeconds = context.message.voice.duration
+  if (durationSeconds > maxDurationSeconds) {
+    await context.reply(`Max duration: ${maxDurationSeconds} seconds (provided: ${durationSeconds})`)
+    return
+  }
+
+  const mimeType = context.message.voice.mime_type
+  if (!mimeType) {
+    await context.reply('Could not determine file mime type')
+    // TODO: log
+    return
+  }
+  if (!supportedMimeTypes.includes(mimeType)) {
+    await context.reply(`Unsupported mime type: ${mimeType}`)
+    // TODO: log
+    return
+  }
+
+  const sizeBytes = context.message.voice.file_size
+  if (!sizeBytes) {
+    await context.reply('Could not determine file size')
+    return
+  }
+  if (sizeBytes > maxSizeBytes) {
+    await context.reply(`Max size: ${maxSizeBytes} bytes (provided: ${sizeBytes})`)
+    return
+  }
+
+  // TODO:
+})
+
+telegraf.on(message('audio'), async context => {
+  const durationSeconds = context.message.audio.duration
+  if (durationSeconds > maxDurationSeconds) {
+    await context.reply(`Max duration: ${maxDurationSeconds} seconds (provided: ${durationSeconds})`)
+    return
+  }
+
+  const mimeType = context.message.audio.mime_type
+  if (!mimeType) {
+    await context.reply('Could not determine file mime type')
+    // TODO: log
+    return
+  }
+  if (!supportedMimeTypes.includes(mimeType)) {
+    await context.reply(`Unsupported mime type: ${mimeType}`)
+    // TODO: log
+    return
+  }
+
+  const sizeBytes = context.message.audio.file_size
+  if (!sizeBytes) {
+    await context.reply('Could not determine file size')
+    return
+  }
+  if (sizeBytes > maxSizeBytes) {
+    await context.reply(`Max size: ${maxSizeBytes} bytes (provided: ${sizeBytes})`)
+    return
+  }
+
+  // TODO:
+})
+
+telegraf.on(message('sticker'), async context => {
+  if (context.message.sticker.type !== 'regular') {
+    await context.reply('Masks and custom emojis are not supported')
+    return
+  }
+
+  if (context.message.sticker.is_animated) {
+    await context.reply('Animated stickers are not supported')
+    return
+  }
+
+  const sizeBytes = context.message.sticker.file_size
+  if (!sizeBytes) {
+    await context.reply('Could not determine file size')
+    return
+  }
+  if (sizeBytes > maxSizeBytes) {
+    await context.reply(`Max size: ${maxSizeBytes} bytes (provided: ${sizeBytes})`)
+    return
+  }
+
+  // TODO:
+})
+
+telegraf.on(message('photo'), async context => {
+  const photo = context.message.photo
+    .sort((a, b) => b.width * b.height - a.width * a.height)
+    .filter(p => p.file_size && p.file_size > maxSizeBytes && p.width <= maxWidth && p.height <= maxHeight)[0]
+  if (!photo) {
+    await context.reply('Photo is too large or invalid')
+    return
+  }
+
+  // TODO:
+})
+
+telegraf.on(message('video_note'), async context => {
+  const durationSeconds = context.message.video_note.duration
+  if (durationSeconds > maxDurationSeconds) {
+    await context.reply(`Max duration: ${maxDurationSeconds} seconds (provided: ${durationSeconds})`)
+    return
+  }
+
+  const sizeBytes = context.message.video_note.file_size
+  if (!sizeBytes) {
+    await context.reply('Could not determine file size')
+    return
+  }
+  if (sizeBytes > maxSizeBytes) {
+    await context.reply(`Max size: ${maxSizeBytes} bytes (provided: ${sizeBytes})`)
+    return
+  }
+
+  const diameter = context.message.video_note.length
+  if (diameter > maxDiameter) {
+    await context.reply(`Max diameter: ${maxDiameter} (provided: ${diameter})`)
+    return
+  }
+
+  // TODO:
+})
+
+telegraf.on(message('video'), async context => {
   const durationSeconds = context.message.video.duration
   if (durationSeconds > maxDurationSeconds) {
     await context.reply(`Max duration: ${maxDurationSeconds} seconds (provided: ${durationSeconds})`)
@@ -17,12 +145,11 @@ telegraf.on('video', async context => {
 
   const sizeBytes = context.message.video.file_size
   if (!sizeBytes) {
-    await context.reply('Could not determine video size')
+    await context.reply('Could not determine file size')
     return
   }
-
   if (sizeBytes > maxSizeBytes) {
-    await context.reply(`Max size: ${maxSizeBytes} bytes (provider: ${sizeBytes})`)
+    await context.reply(`Max size: ${maxSizeBytes} bytes (provided: ${sizeBytes})`)
     return
   }
 
@@ -40,16 +167,17 @@ telegraf.on('video', async context => {
 
   const mimeType = context.message.video.mime_type
   if (!mimeType) {
-    await context.reply('Could not determine video mime type')
+    await context.reply('Could not determine file mime type')
+    // TODO: log
     return
   }
-
   if (!supportedMimeTypes.includes(mimeType)) {
     await context.reply('Unsupported mime type')
+    // TODO: log
     return
   }
 
-  // TODO: process video
+  // TODO:
 })
 
 telegraf.launch()
